78930648539b51e50b03fc9c7da53fdbda46dfb8
William Roger <williamroger.frontend@gmail.com>
2024-06-21 22:28:44 -0300

    finalizado heade tela de exercícios

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,40 @@
-import { Center, Text } from '@gluestack-ui/themed';
+import { HStack, Heading, Icon, VStack, Text } from '@gluestack-ui/themed';
+import { TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
+import BodySvg from '@assets/body.svg';
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1} bg="$trueGray700">
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1} bg="$trueGray700">
+      <VStack px="$8" bg="$trueGray600" pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon 
+            as={Feather}
+            name="arrow-left"
+            color="$trueGray300"
+            size="xl"
+          />
+        </TouchableOpacity>
+        <HStack justifyContent="space-between" alignItems="center" mt="$4" mb="$8">
+          <Heading color="$trueGray100" fontSize="$lg" flexShrink={1}>
+            Puxada frontal
+          </Heading>
+          <HStack>
+            <BodySvg />
+            <Text color="$trueGray200" ml="$1" textTransform="capitalize">Costas</Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { VStack, FlatList, HStack, Heading, Text } from '@gluestack-ui/themed';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
@@ -9,6 +12,12 @@ export function Home() {
   const [groups, setGroups] = useState(['costas', 'bíceps', 'tríceps', 'ombro']);
   const [groupSelected, setGroupSelected] = useState('costas');
   const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra']);
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
+
   return (
     <VStack flex={1} bg="$trueGray700">
       <HomeHeader />
@@ -44,7 +53,7 @@ export function Home() {
           data={exercises}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
-            <ExerciseCard />
+            <ExerciseCard onPress={handleOpenExerciseDetails}/>
           )}
           showsVerticalScrollIndicator={false}
         />
