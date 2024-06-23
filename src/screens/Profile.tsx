9c8a74594a1d8c69b7f38f1802bb8a8fb5b630dd
William Roger <williamroger.frontend@gmail.com>
2024-06-23 00:05:28 -0300

    adicionado método para carregamento de imagem de perfil do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,52 @@
 import { TouchableOpacity } from 'react-native';
-import { VStack, Text, ScrollView, Center, Heading } from '@gluestack-ui/themed';
+import { VStack, Text, ScrollView, Center, Heading, useToast, Toast, ToastTitle } from '@gluestack-ui/themed';
+import * as ImagePicker from 'expo-image-picker';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { useState } from 'react';
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState('https://avatars.githubusercontent.com/u/3818294?v=4');
+  const toast = useToast();
+  
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+      
+      if (photoSelected.assets[0].uri) {
+        /* verifica se existe a propriedade fileSize e se o tamanho é maior que 5mb */
+        if (photoSelected.assets[0].fileSize && (photoSelected.assets[0].fileSize / 1024 / 1024) > 1) {
+          return toast.show({
+            placement: 'top',
+            render: () => (
+              <Toast action="error" variant="accent">
+                <ToastTitle>
+                  {'Essa imagem é muito grande. Escolha uma imagem de até 5MB.'}
+                </ToastTitle>
+              </Toast>
+            )
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1} bg="$trueGray700">
       <ScreenHeader title="Perfil" />
@@ -14,10 +54,10 @@ export function Profile() {
       <ScrollView>
         <Center mt="$6" px="$10">
           <UserPhoto 
-            imageUrl={'https://avatars.githubusercontent.com/u/3818294?v=4'}
+            imageUrl={userPhoto}
             size="2xl" 
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text 
               color="$green500" 
               fontFamily="$heading" 
